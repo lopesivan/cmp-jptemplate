5b6e9608df2975f05590cca995be33337140f693
Ivan Lopes <lopesivan.ufrj@gmail.com>
2023-03-06 20:18:27 -0300

    Atualiza o arquivo lua/cmp_jptemplate/init.lua

--- lua/cmp_jptemplate/init.lua
+++ lua/cmp_jptemplate/init.lua
@@ -4,54 +4,53 @@ local util = require('vim.lsp.util')
 
 local source = {}
 source.new = function()
-	return setmetatable({}, { __index = source })
+    return setmetatable({}, { __index = source })
 end
 
 source.get_keyword_pattern = function()
-	return '\\%([^[:alnum:][:blank:]]\\|\\w\\+\\)'
+    return '\\%([^[:alnum:][:blank:]]\\|\\w\\+\\)'
 end
 
 function source:get_debug_name()
-	return 'jptemplate'
+    return 'jptemplate'
 end
 
 function source:complete(_, callback)
+    local path = string.format(
+        "%s/jptemplate/%s",
+        vim.fn.stdpath "config",
+        vim.bo.filetype)
 
-	local path = string.format(
-		"%s/%s",
-		vim.fn.expand('~/.config/nvim/jptemplate/'),
-		vim.bo.filetype)
+    local paths = vim.fn.globpath(path, "*", true, true)
 
-	local paths = vim.fn.globpath(path, "*", true, true)
+    local snippets = {}
+    for _, file in ipairs(paths) do
+        local snippet = vim.fn.fnamemodify(file, ":t:r")
+        table.insert(snippets, snippet)
+    end
 
-	local snippets = {}
-	for _, file in ipairs(paths) do
-		local snippet = vim.fn.fnamemodify(file, ":t:r")
-		table.insert(snippets, snippet)
-	end
+    local items = {}
+    for _, value in pairs(snippets) do
+        local item = {
+            word = value,
+            label = value,
+            user_data = value,
+            kind = cmp.lsp.CompletionItemKind.Snippet,
+        }
+        items[#items + 1] = item
+    end
 
-	local items = {}
-	for _, value in pairs(snippets) do
-		local item = {
-			word = value,
-			label = value,
-			user_data = value,
-			kind = cmp.lsp.CompletionItemKind.Snippet,
-		}
-		items[#items + 1] = item
-	end
-
-	callback(items)
+    callback(items)
 end
 
 function source:execute(completion_item, callback)
-	vim.call('JptemplateExpandSnippet')
-	callback(completion_item)
+    vim.call('JptemplateExpandSnippet')
+    callback(completion_item)
 end
 
 function source:is_available()
-	-- if UltiSnips is installed then this variable should be defined
-	return true
+    -- if UltiSnips is installed then this variable should be defined
+    return true
 end
 
 return source
